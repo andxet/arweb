0193ec57a9e0a4f75f9ebf3545abd459a04e474b
Andrea Peretti <and.xet@gmail.com>
2019-10-26 17:04:55 +0200

    tests

---
+++ business-card/README.md
@@ -0,0 +1,12 @@
+# business-card
+
+business-card.
+
+Built with [A-Frame](https://aframe.io).
+
+## Setup
+
+```sh
+npm install
+npm run start
+```
---
+++ radial-animation.js
@@ -0,0 +1,15 @@
+AFRAME.registerComponent('radial-animation', {
+  schema: {
+    foo: {type: 'string'}
+  },
+
+  init: function () {
+    var el = this.el;
+    var data = this.data;
+  },
+
+  update: function () {
+    var el = this.el;
+    var data = this.data;
+  }
+});
